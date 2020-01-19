8b683bfa78e8e2bbe5d6c470fabc07785fa9c974
Alexander Borisov <nillerprog@gmail.com>
2020-01-19 15:52:43 +0300

    Simple arena editor

---
+++ Assets/Editor/ArenaEditorWindow.cs
@@ -0,0 +1,77 @@
+﻿using Assets.Scripts.Configs.Arena;
+using JetBrains.Annotations;
+using UnityEditor;
+using UnityEngine;
+
+public class ArenaEditorWindow : EditorWindow
+{
+    private static ArenaEditorWindow _window;
+
+    private ArenaConfig _arenaConfig;
+    private int _currentArenaIndex;
+    private ArenaData _currentArenaData;
+    private int[] _arenaIndexes;
+    private string[] _arenaIndexDisplays;
+
+    [UsedImplicitly]
+    [MenuItem("Tools/Roguelike/Arena editor")]
+    private static void Open()
+    {
+        if (_window != null)
+        {
+            _window.Focus();
+            return;
+        }
+
+        _window = GetWindow<ArenaEditorWindow>();
+        _window.titleContent = new GUIContent("Arena Editor", EditorGUIUtility.FindTexture("d_ViewToolOrbit On"));
+    }
+
+    private void OnEnable()
+    {
+
+    }
+
+    private void OnGUI()
+    {
+        EditorGUI.BeginChangeCheck();
+        _arenaConfig =
+            EditorGUILayout.ObjectField("Arena config", _arenaConfig, typeof(ArenaConfig), false) as ArenaConfig;
+        if (EditorGUI.EndChangeCheck())
+        {
+            if (_arenaConfig != null)
+            {
+                _arenaIndexes = new int[_arenaConfig.Arenas.Length];
+                _arenaIndexDisplays = new string[_arenaConfig.Arenas.Length];
+                for (int i = 0; i < _arenaIndexes.Length; i++)
+                {
+                    _arenaIndexes[i] = i;
+                    _arenaIndexDisplays[i] = i.ToString();
+                }
+            }
+
+            
+        }
+
+        if (_arenaConfig == null || _arenaConfig.Arenas.Length == 0)
+        {
+            return;
+        }
+
+        _currentArenaIndex = EditorGUILayout.IntPopup("Arena", _currentArenaIndex, _arenaIndexDisplays, _arenaIndexes);
+        _currentArenaData = _arenaConfig.Arenas[_currentArenaIndex];
+
+        if (GUILayout.Button("Open editor"))
+        {
+            CellsEditorWindow.Open(_currentArenaData);
+        }
+
+        if (GUILayout.Button("Save"))
+        {
+            EditorUtility.SetDirty(_arenaConfig);
+            AssetDatabase.Refresh();
+        }
+  
+    }
+
+}
---
+++ Assets/Editor/CellsEditorWindow.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Assets.Scripts.Configs.Arena;
+using UnityEditor;
+using UnityEngine;
+
+public class CellsEditorWindow : EditorWindow
+{
+    private const int CellSize = 20;
+    private const int LeftSideWidth = 150;
+    private static CellsEditorWindow _window;
+    private ArenaData _arenaData;
+    private ArenaCell _currentCell;
+
+    public static void Open(ArenaData arenaData)
+    {
+        if (_window != null)
+        {
+            _window.Focus();
+            return;
+        }
+
+        _window = GetWindow<CellsEditorWindow>();
+        _window.titleContent = new GUIContent("Cells Editor", EditorGUIUtility.FindTexture("d_ViewToolOrbit On"));
+        _window._arenaData = arenaData;
+        _window.minSize = _window.maxSize = arenaData.Size * CellSize + new Vector2(LeftSideWidth, 0);
+        
+    }
+
+    private void OnGUI()
+    {
+        var fullRect = new Rect(0, 0, position.width - LeftSideWidth, position.height);
+        Handles.DrawSolidRectangleWithOutline(fullRect, Color.white, Color.black);
+        var lines = new Vector3[_arenaData.Size.x * 2 + _arenaData.Size.y * 2]; 
+        for (int i = 0; i < _arenaData.Size.x; i++)
+        {
+            lines[i*2] = new Vector3(CellSize * i, 0, 0);
+            lines[i*2 + 1] = new Vector3(CellSize * i, fullRect.yMax, 0);
+        }
+
+        var offset = _arenaData.Size.x * 2;
+        for (int i = 0; i < _arenaData.Size.y; i++)
+        {
+            lines[offset + i * 2] = new Vector3(0, CellSize * i, 0);
+            lines[offset + i * 2 + 1] = new Vector3(fullRect.xMax, CellSize * i, 0);
+        }
+
+        Handles.color = Color.black;
+        Handles.DrawLines(lines);
+
+        for (int i = 0; i < _arenaData.Cells.Length; i++)
+        {
+            var index = TransformCellIndex(new Vector2Int(i % _arenaData.Size.x, i / _arenaData.Size.x));
+
+            var cell = _arenaData.Cells[i];
+
+            Color? color = null;
+            switch (cell.Type)
+            {
+                case ArenaCellType.PlayerSpawn:
+                    color = Color.green;
+                    break;
+                case ArenaCellType.Obstacle:
+                    color = Color.gray;
+                    break;
+            }
+
+            if (color != null)
+            {
+                Handles.color = color.Value;
+                Handles.DrawSolidRectangleWithOutline(
+                    new Rect(index.x * CellSize, index.y * CellSize, CellSize, CellSize), 
+                    color.Value, Color.black);
+            }
+
+            if (_currentCell != null)
+            {
+                GUILayout.BeginArea(new Rect(fullRect.xMax, 0, LeftSideWidth, fullRect.height));
+                DrawCellInfo();
+                GUILayout.EndArea();
+            }
+
+            var evt = Event.current;
+            if (evt.type == EventType.MouseUp && evt.button == 0)
+            {
+                var pos = evt.mousePosition;
+                if (!fullRect.Contains(pos))
+                {
+                    return;
+                }
+
+                var cellIndex = TransformCellIndex(new Vector2Int((int)(pos.x / CellSize), (int)(pos.y / CellSize)));
+                var newCell = _arenaData.Cells[cellIndex.x + cellIndex.y * _arenaData.Size.x];
+
+                if (evt.control)
+                {
+                    newCell.Type = _currentCell.Type;
+                }
+
+                _currentCell = newCell;
+
+                evt.Use();
+            }
+        }
+    }
+
+    private void DrawCellInfo()
+    {
+        _currentCell.Type = (ArenaCellType)EditorGUILayout.EnumPopup(_currentCell.Type);
+    }
+
+    private Vector2Int TransformCellIndex(Vector2Int cellIndex)
+    {
+        return new Vector2Int(cellIndex.x, _arenaData.Size.y - cellIndex.y - 1);
+    }
+}
